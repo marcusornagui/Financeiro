e30a75bb693d25863f76dcf4a90f80b252c54dc1
Marcus Ornagui <marcus.ornagui@vrsoft.com.br>
2022-11-14 21:23:47 -0300

    -Iniciando validação de componentes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -28,7 +28,8 @@ import { EditarTransacaoComponent } from './componentes/transacoes/editar-transa
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/componentes/transacoes/criar-transacao/criar-transacao.component.ts
+++ src/app/componentes/transacoes/criar-transacao/criar-transacao.component.ts
@@ -3,6 +3,7 @@ import { Transacao } from './../transacoes';
 import { Component, OnInit } from '@angular/core';
 import { TransacaoService } from '../transacao.service';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-transacao',
@@ -11,28 +12,46 @@ import { Router } from '@angular/router';
 })
 export class CriarTransacaoComponent implements OnInit {
 
-  transacao: Transacao = {
-    conteudo: '',
-    valor: 0,
-    modelo: ''
-  }
+  formulario!: FormGroup
 
   constructor(
     private service: TransacaoService,
-    private router: Router
-    ) { }
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.formulario = this.formBuilder.group({
+      conteudo: ['', Validators.compose([
+        Validators.required,
+        Validators.pattern(/(.|\s)*\S(.|\s)*/)
+      ])],
+      valor: ['', Validators.compose([
+        Validators.required,
+        Validators.maxLength(8)
+      ])],
+      modelo: ['despesa']
+    })
   }
 
   criarTransacao() {
-    this.service.criar(this.transacao).subscribe(() => {
-      this.router.navigate(['/listarTransacao'])
-    })
+    if (this.formulario.valid) {
+      this.service.criar(this.formulario.value).subscribe(() => {
+        this.router.navigate(['/listarTransacao'])
+      })
+    }
   }
 
   cancelar() {
     this.router.navigate(['/listarTransacao'])
+  }
+
+  habilitarBotao(): string {
+    if (this.formulario.valid) {
+      return 'botao'
+    } else {
+      return 'botao__desabilitado'
+    }
   }
 
 }
