f8cf06b4cb3ae306dae4bb2cda8dab6070411fef
Marcus Ornagui <marcusornagui@gmail.com>
2022-11-29 20:39:42 -0300

    -Ajuste formatação campo moeda.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -13,6 +13,11 @@ import { TransacaoComponent } from './componentes/transacoes/transacao/transacao
 import { ExcluirTransacaoComponent } from './componentes/transacoes/excluir-transacao/excluir-transacao.component';
 import { EditarTransacaoComponent } from './componentes/transacoes/editar-transacao/editar-transacao.component';
 import { BotaoCarregarMaisComponent } from './componentes/transacoes/listar-transacoes/botao-carregar-mais/botao-carregar-mais.component';
+
+import ptBr from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(ptBr);
 
 @NgModule({
   declarations: [
@@ -33,7 +38,10 @@ import { BotaoCarregarMaisComponent } from './componentes/transacoes/listar-tran
     HttpClientModule,
     ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt' },
+    { provide: DEFAULT_CURRENCY_CODE, useValue: 'BRL' }
+ ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
